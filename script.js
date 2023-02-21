ae8308cc11c59ab9acb4cd4c9fb866476faa279e
Semyon Berdov <berdov1894@gmail.com>
2023-02-21 20:05:13 +0600

    intermediate minor changes at the end of the day

--- script.js
+++ script.js
@@ -52,16 +52,81 @@ const Questions = [
     ],
   },
 ];
+/**
+ * ! функция repeat
+ * записывает в html вопрос и вариант ответа
+ * при нажатии на кнопку варианта подсвечивает её
+ * ! функция next
+ * todo при нажатии на кноку "Next" записывает баллы за ответ в переменную score
+ * todo на последнем вопросе кнопку "Next" заменяет кнопка "Show result"
+ * todo при нажатии на "Show result" на странице отображает результат теста
+ * */
 
-// function repeat(id)
-const question = document.getElementById("question");
+function repeat(id) {
+  //* Show question text & options
 
-question.innerText = Questions[0].q;
+  const question = document.getElementById("question");
 
-const answerA = document.getElementById("option1");
-const answerB = document.getElementById("option2");
-const answerC = document.getElementById("option3");
+  question.innerText = Questions[id].q;
 
-answerA.innerText = Questions[0].a[0].text;
-answerB.innerText = Questions[0].a[1].text;
-answerC.innerText = Questions[0].a[2].text;
+  const answerA = document.getElementById("option1");
+  const answerB = document.getElementById("option2");
+  const answerC = document.getElementById("option3");
+
+  answerA.innerText = Questions[id].a[0].text;
+  answerB.innerText = Questions[id].a[1].text;
+  answerC.innerText = Questions[id].a[2].text;
+
+  //* Coloring selected button
+  answerA.addEventListener("click", () => {
+    answerA.style.backgroundColor = "#E072A4";
+    answerB.style.backgroundColor = "#88df60";
+    answerC.style.backgroundColor = "#88df60";
+  });
+
+  answerB.addEventListener("click", () => {
+    answerA.style.backgroundColor = "#88df60";
+    answerB.style.backgroundColor = "#E072A4";
+    answerC.style.backgroundColor = "#88df60";
+  });
+
+  answerC.addEventListener("click", () => {
+    answerA.style.backgroundColor = "#88df60";
+    answerB.style.backgroundColor = "#88df60";
+    answerC.style.backgroundColor = "#E072A4";
+  });
+}
+//* Declaring variables, that count score of each factor
+let scoreMD = 0;
+let scoreA = 0;
+let scoreB = 0;
+let scoreC = 0;
+let scoreE = 0;
+let scoreF = 0;
+let scoreG = 0;
+let scoreH = 0;
+let scoreI = 0;
+let scoreL = 0;
+let scoreM = 0;
+let scoreN = 0;
+let scoreO = 0;
+let scoreQ1 = 0;
+let scoreQ2 = 0;
+let scoreQ3 = 0;
+let scoreQ4 = 0;
+//* Declaring variable that count questions
+let currentQuestionNumber = 0;
+
+repeat(currentQuestionNumber);
+//*Get next buton
+const next = document.getElementById("next-button");
+
+//* при нажатии на кноку "Next" обновляет вопрос и варианты ответов
+next.addEventListener("click", (id) => {
+  if (currentQuestionNumber < 104) {
+    currentQuestionNumber++;
+    repeat(currentQuestionNumber);
+  } else {
+    console.log(score); //todo дописать функцию чтобы заменяла кнопку и считала очки
+  }
+});
